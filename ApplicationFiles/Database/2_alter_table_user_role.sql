adf9e4df8746ad77b4dd883d0616bc5d4627ae86
trancuong <trancuong052017@gmail.com>
2018-02-07 01:09:38 +0100

    thu commit lan 4

--- ApplicationFiles/Database/2_alter_table_user_role.sql
+++ ApplicationFiles/Database/2_alter_table_user_role.sql
@@ -2,7 +2,7 @@ use toeiconline;
 
 ALTER TABLE user MODIFY COLUMN name VARCHAR(255) NOT NULL;
 ALTER TABLE user MODIFY COLUMN password VARCHAR(255) NOT NULL;
-ALTER TABLE user MODIFY COLUMN roleid BIGINT(20) NOT NULL;
+//ALTER TABLE user MODIFY COLUMN roleid BIGINT(20) NOT NULL;
 
 alter table user add column acce int(10);
 alter table user add column roleid bigint;
