d24d434300cdbac870a70880880ec3b30f69315c
trancuong <trancuong052017@gmail.com>
2018-02-07 01:15:10 +0100

    thu commit lan 5

--- ApplicationFiles/Database/2_alter_table_user_role.sql
+++ ApplicationFiles/Database/2_alter_table_user_role.sql
@@ -4,6 +4,6 @@ ALTER TABLE user MODIFY COLUMN name VARCHAR(255) NOT NULL;
 ALTER TABLE user MODIFY COLUMN password VARCHAR(255) NOT NULL;
 //ALTER TABLE user MODIFY COLUMN roleid BIGINT(20) NOT NULL;
 
-alter table user add column acce int(10);
+//alter table user add column acce int(10);
 alter table user add column roleid bigint;
 alter table user add constraint fk_user_role foreign key(roleid) references role(roleid) ;
